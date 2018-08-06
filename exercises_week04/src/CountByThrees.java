a25441a2962d57aa066627766431abf5967e1dee
Sherin24 <sherinsarah.varghese@my.jcu.edu.au>
2018-08-06 10:37:00 +0800

    Exercises of week 4 chap 6

---
+++ exercises_week04/src/CountByThrees.java
@@ -0,0 +1,15 @@
+public class CountByThrees
+{
+    public static void main(String args[])
+    {
+        int i;
+        for (i= 3; i<=300; i++)
+        {
+            System.out.print(i + " ");
+            if (i%30 == 0)
+            {
+                System.out.println();
+            }
+        }
+    }
+}
